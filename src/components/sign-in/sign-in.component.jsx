dead58b0abaa1e179f36bfd210d21f9d2251b071
Shifath Salam <shifath.salam.216@my.csun.edu>
2020-01-02 22:15:29 -0800

    implemented firebase utils, included ability to store authenticated users into db

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import FormInput from "../../components/form-input/form-input.component";
-import CustomButton from "../../components/custom-buttom/custom-button.component";
+import CustomButton from "../../components/custom-button/custom-button.component";
 
 import { signInWithGoogle } from "../../firebase/firebase.utils";
 
